8ac7e8ee79d57971a85995709a5b5c248d5ab112
Krzysztof Gumuła <k.gumula@icloud.com>
2020-02-14 21:17:02 +0100

    add class active

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
   })*?
 /*DO WYSZUKANIA WSZYSTKICH LINKOW a W HTML DODAC GUZIK TEST BUTTON*/
 const titleClickHandler = function (event) {
+    const clickedElement = this;
     console.log('Link was clicked!', event);
 
     /* remove class 'active' from all article links  */
@@ -14,7 +15,8 @@ const titleClickHandler = function (event) {
         activeLink.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
-
+    console.log('clickedElement:', clickedElement);
+    activeLink.classList.add('active');
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts post.active');
 
