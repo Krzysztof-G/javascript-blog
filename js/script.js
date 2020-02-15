93b58fac41087591ec91e7254ebcc73ed4c35862
Krzysztof Gumuła <k.gumula@icloud.com>
2020-02-15 16:25:51 +0100

    add article change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 'use strict';
-/*document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  })*?
-/*DO WYSZUKANIA WSZYSTKICH LINKOW a W HTML DODAC GUZIK TEST BUTTON*/
+
 const titleClickHandler = function (event) {
     const clickedElement = this;
+    event.preventDefault();
     console.log('Link was clicked!', event);
 
     /* remove class 'active' from all article links  */
@@ -16,19 +13,22 @@ const titleClickHandler = function (event) {
     }
     /* add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
-    activeLink.classList.add('active');
+    clickedElement.classList.add('active');
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts post.active');
 
     for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
+    activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-
+    const href = clickedElement.getAttribute('href');
+    console.log('href is', href);
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(href);
+    console.log('targetArticle is', targetArticle);
     /* add class 'active' to the correct article */
-}
+    targetArticle.classList.add('active');
+}   
 
 const links = document.querySelectorAll('.titles a');
 
